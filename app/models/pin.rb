986c83c72a46a892152150e38d1c5b7a21c685b0
Neville Davey <nevilledavey@gmail.com>
2013-07-30 17:14:48 -0700

    Add jQuery Masonry

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_file_name
 
   validates :description, presence: true
   validates :user_id, presence: true
